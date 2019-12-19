541582e8ade5c37bc23129690d009c76ad1e73e3
unknown <bilelmek@gmail.com>
2019-12-19 01:33:37 +0100

    conf server + discovery + feign

---
+++ currency-exchange-service/src/main/java/com/bilelmk/currencyexchangeservice/ExchangeValue.java
@@ -0,0 +1,72 @@
+package com.bilelmk.currencyexchangeservice;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+public class ExchangeValue {
+
+    @Id
+    private Long id ;
+
+    @Column(name = "currency_from")
+    private String from ;
+
+    @Column(name = "currency_to")
+    private String to ;
+
+    private BigDecimal conversionMultiple ;
+    private int port;
+
+    public ExchangeValue(Long id, String from, String to, BigDecimal conversionMultiple) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+    }
+
+    public ExchangeValue() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public BigDecimal getConversionMultiple() {
+        return conversionMultiple;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public void setConversionMultiple(BigDecimal conversionMultiple) {
+        this.conversionMultiple = conversionMultiple;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public int getPort() {
+        return port;
+    }
+}
---
+++ eureka-naming-service/src/test/java/com/bilelmk/eurekanamingservice/EurekaNamingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilelmk.eurekanamingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaNamingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
